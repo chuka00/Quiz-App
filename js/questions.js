0e3eaba3d3e256ab8e2c3a4d799fdf01221fac21
Victor Chukwuka Ogbodo <81800355+chuka00@users.noreply.github.com>
2022-09-03 23:44:25 +0100

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,178 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "script",
+    options: [
+      "JS",
+      "script",
+      "scripting",
+      "javascript"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Who is making the Web standards?",
+    answer: "The World Wide Web Consortium",
+    options: [
+      "Chrome",
+      "Mozilla",
+      "The World Wide Web Consortium",
+      "Microsoft"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Choose the correct HTML element for the largest heading:",
+    answer: "h1",
+    options: [
+      "h6",
+      "head",
+      "heading",
+      "h1"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "What is the correct HTML element for inserting a line break?",
+    answer: "br",
+    options: [
+      "break",
+      "br",
+      "lb",
+      "line"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answer: "onclick",
+    options: [
+      "onmouseclick",
+      "onchange",
+      "onmouseover",
+      "onclick"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "How can you add a comment in a JavaScript?",
+    answer: "//This is a comment",
+    options: [
+      "//This is a comment",
+      "!This is a comment!",
+      "This is a comment",
+      "!--This is a comment--!"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "Where in an HTML document is the correct place to refer to an external style sheet?",
+    answer: "In the head section",
+    options: [
+      "In the body section",
+      "At the end of the document",
+      "In the head section",
+      "In the tail section"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "How does a WHILE loop start?",
+    answer: "while(i <= 10)",
+    options: [
+      "while(i <= 10; i++)",
+      "while(i <= 10)",
+      "while i = 1 to 10",
+      "while(i++ = 10)"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Which HTML tag is used to define an internal style sheet?",
+    answer: "style",
+    options: [
+      "sheet",
+      "script",
+      "css",
+      "style"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "JavaScript is the same as Java.",
+    answer: "Disagree",
+    options: [
+      "Agree",
+      "Not sure",
+      "Strongly Agree",
+      "Disagree"
+    ]
+  },
+];
